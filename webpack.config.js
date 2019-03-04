cb4e7f4db215807169d8952af6750a255484d494
Francesco Esposito <fraesps@gmail.com>
2019-03-04 19:34:51 +0100

    adds yarn lock and deletes docs folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,57 @@
-const { join, } = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
+const { join } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
 
 module.exports = (env, argv) => {
-  const DEVELOPMENT = argv.mode === 'development';
+  const DEVELOPMENT = argv.mode === "development";
 
   return {
-    devtool: DEVELOPMENT ? 'cheap-eval-source-map' : false,
-    output: {
-      path: join(__dirname, 'docs'), // for github pages
-      filename: 'bundle.js'
-    },
+    devtool: DEVELOPMENT ? "cheap-eval-source-map" : false,
     module: {
       rules: [
         {
           test: /\.html$/,
           use: {
-            loader: 'html-loader'
+            loader: "html-loader"
           }
         },
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"]
         },
         {
           test: /\.svg$/,
           use: [
             {
-              loader: 'svg-sprite-loader',
-            },
-          ],
+              loader: "svg-sprite-loader"
+            }
+          ]
         },
         {
           test: /\.(png|jpg|jpeg|gif)$/,
           use: [
             {
-              loader: 'file-loader',
-              options: {},
-            },
-          ],
-        },
-      ],
+              loader: "file-loader",
+              options: {}
+            }
+          ]
+        }
+      ]
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './src/index.html',
+        template: "./src/index.html"
       }),
       new CopyWebpackPlugin([
-        { from: './src/assets', to: 'assets' },
-        { from: './src/resume', to: 'resume' },
-        { from: './src/favicon.ico', to: 'favicon.ico' },
+        { from: "./src/assets", to: "assets" },
+        { from: "./src/resume", to: "resume" },
+        { from: "./src/favicon.ico", to: "favicon.ico" }
       ]),
       new SpriteLoaderPlugin()
     ],
     devServer: {
-      contentBase: join(__dirname, 'docs'),
-    },
+      contentBase: join(__dirname, "docs")
+    }
   };
 };
